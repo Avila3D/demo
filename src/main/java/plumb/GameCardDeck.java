0546f7b09d7dc1a3dd6db25871306734e064dc70
Avila3D <tony@avila3d.com>
2019-02-28 22:52:06 -0700

    displayed 56 playing cards(deck) for Assignment 6

---
+++ src/main/java/plumb/GameCardDeck.java
@@ -0,0 +1,46 @@
+/**
+ * This class implements the PDeck Interface to create a deck of PCards
+ * @ author Antonio Avila
+ * 2/27/19
+ */
+
+package plumb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameCardDeck implements PDeck {
+
+    // create list to hold cards
+    List<PCard> deck = new ArrayList<PCard>();
+
+
+    @Override
+    public void shuffle() {
+
+    }
+    @Override
+    public void addCard(PCard card) {
+        deck.add(card);
+    }
+    @Override
+    public PCard dealCard() {
+        PCard cardToDeal = deck.get(-0);
+        deck.remove(-0);
+        return cardToDeal;
+    }
+    @Override
+    public PCard dealHiddenCard() {
+        return null;
+    }
+    @Override
+    public int cardCount() {
+        return deck.size();
+    }
+
+
+}
+
+
+
+
---
+++ src/test/java/test/DeckMain.java
@@ -0,0 +1,135 @@
+package test;
+
+import plumb.GameCard;
+import plumb.GameCardDeck;
+import plumb.PCard;
+
+public class DeckMain {
+
+    public static void main(String[] args) {
+
+        GameCardDeck gameCardDeck = new GameCardDeck();
+
+        // hearts
+        GameCard aceOfHearts = new GameCard("A \u2665");
+        GameCard twoOfHearts = new GameCard("2 \u2665");
+        GameCard threeOfHearts = new GameCard("3 \u2665");
+        GameCard fourOfHearts = new GameCard("4 \u2665");
+        GameCard fiveOfHearts = new GameCard("5 \u2665");
+        GameCard sixOfHearts = new GameCard("6 \u2665");
+        GameCard sevenOfHearts = new GameCard("7 \u2665");
+        GameCard eightOfHearts = new GameCard("8 \u2665");
+        GameCard nineOfHearts = new GameCard("9 \u2665");
+        GameCard tenOfHearts = new GameCard("10 \u2665");
+        GameCard jackOfHearts = new GameCard("J \u2665");
+        GameCard queenOfHearts = new GameCard("Q \u2665");
+        GameCard kingOfHearts = new GameCard("K \u2665");
+        gameCardDeck.addCard(aceOfHearts);
+        gameCardDeck.addCard(twoOfHearts);
+        gameCardDeck.addCard(threeOfHearts);
+        gameCardDeck.addCard(fourOfHearts);
+        gameCardDeck.addCard(fiveOfHearts);
+        gameCardDeck.addCard(sixOfHearts);
+        gameCardDeck.addCard(sevenOfHearts);
+        gameCardDeck.addCard(eightOfHearts);
+        gameCardDeck.addCard(nineOfHearts);
+        gameCardDeck.addCard(tenOfHearts);
+        gameCardDeck.addCard(jackOfHearts);
+        gameCardDeck.addCard(queenOfHearts);
+        gameCardDeck.addCard(kingOfHearts);
+
+        // clubs
+        GameCard aceOfClubs = new GameCard("A \u2663");
+        GameCard twoOfClubs = new GameCard("2 \u2663");
+        GameCard threeOfClubs = new GameCard("3 \u2663");
+        GameCard fourOfClubs = new GameCard("4 \u2663");
+        GameCard fiveOfClubs = new GameCard("5 \u2663");
+        GameCard sixOfClubs = new GameCard("6 \u2663");
+        GameCard sevenOfClubs = new GameCard("7 \u2663");
+        GameCard eightOfClubs = new GameCard("8 \u2663");
+        GameCard nineOfClubs = new GameCard("9 \u2663");
+        GameCard tenOfClubs = new GameCard("10 \u2663");
+        GameCard jackOfClubs = new GameCard("J \u2663");
+        GameCard queenOfClubs = new GameCard("Q \u2663");
+        GameCard kingOfClubs = new GameCard("K \u2663");
+        gameCardDeck.addCard(aceOfClubs);
+        gameCardDeck.addCard(twoOfClubs);
+        gameCardDeck.addCard(threeOfClubs);
+        gameCardDeck.addCard(fourOfClubs);
+        gameCardDeck.addCard(fiveOfClubs);
+        gameCardDeck.addCard(sixOfClubs);
+        gameCardDeck.addCard(sevenOfClubs);
+        gameCardDeck.addCard(eightOfClubs);
+        gameCardDeck.addCard(nineOfClubs);
+        gameCardDeck.addCard(tenOfClubs);
+        gameCardDeck.addCard(jackOfClubs);
+        gameCardDeck.addCard(queenOfClubs);
+        gameCardDeck.addCard(kingOfClubs);
+
+        // diamonds
+        GameCard aceOfDiamonds = new GameCard("A \u25C6");
+        GameCard twoOfDiamonds = new GameCard("2 \u25C6");
+        GameCard threeOfDiamonds = new GameCard("3 \u25C6");
+        GameCard fourOfDiamonds = new GameCard("4 \u25C6");
+        GameCard fiveOfDiamonds = new GameCard("5 \u25C6");
+        GameCard sixOfDiamonds = new GameCard("6 \u25C6");
+        GameCard sevenOfDiamonds = new GameCard("7 \u25C6");
+        GameCard eightOfDiamonds = new GameCard("8 \u25C6");
+        GameCard nineOfDiamonds = new GameCard("9 \u25C6");
+        GameCard tenOfDiamonds = new GameCard("10 \u25C6");
+        GameCard jackOfDiamonds = new GameCard("J \u25C6");
+        GameCard queenOfDiamonds = new GameCard("Q \u25C6");
+        GameCard kingOfDiamonds = new GameCard("K \u25C6");
+        gameCardDeck.addCard(aceOfDiamonds);
+        gameCardDeck.addCard(twoOfDiamonds);
+        gameCardDeck.addCard(threeOfDiamonds);
+        gameCardDeck.addCard(fourOfDiamonds);
+        gameCardDeck.addCard(fiveOfDiamonds);
+        gameCardDeck.addCard(sixOfDiamonds);
+        gameCardDeck.addCard(sevenOfDiamonds);
+        gameCardDeck.addCard(eightOfDiamonds);
+        gameCardDeck.addCard(nineOfDiamonds);
+        gameCardDeck.addCard(tenOfDiamonds);
+        gameCardDeck.addCard(jackOfDiamonds);
+        gameCardDeck.addCard(queenOfDiamonds);
+        gameCardDeck.addCard(kingOfDiamonds);
+
+
+        // spades
+        GameCard aceOfSpades = new GameCard("A \u2660");
+        GameCard twoOfSpades = new GameCard("2 \u2660");
+        GameCard threeOfSpades = new GameCard("3 \u2660");
+        GameCard fourOfSpades = new GameCard("4 \u2660");
+        GameCard fiveOfSpades = new GameCard("5 \u2660");
+        GameCard sixOfSpades = new GameCard("6 \u2660");
+        GameCard sevenOfSpades = new GameCard("7 \u2660");
+        GameCard eightOfSpades = new GameCard("8 \u2660");
+        GameCard nineOfSpades = new GameCard("9 \u2660");
+        GameCard tenOfSpades = new GameCard("10 \u2660");
+        GameCard jackOfSpades = new GameCard("J \u2660");
+        GameCard queenOfSpades = new GameCard("Q \u2660");
+        GameCard kingOfSpades = new GameCard("K \u2660");
+        gameCardDeck.addCard(aceOfSpades);
+        gameCardDeck.addCard(twoOfSpades);
+        gameCardDeck.addCard(threeOfSpades);
+        gameCardDeck.addCard(fourOfSpades);
+        gameCardDeck.addCard(fiveOfSpades);
+        gameCardDeck.addCard(sixOfSpades);
+        gameCardDeck.addCard(sevenOfSpades);
+        gameCardDeck.addCard(eightOfSpades);
+        gameCardDeck.addCard(nineOfSpades);
+        gameCardDeck.addCard(tenOfSpades);
+        gameCardDeck.addCard(jackOfSpades);
+        gameCardDeck.addCard(queenOfSpades);
+        gameCardDeck.addCard(kingOfSpades);
+
+        DeckTest.run(gameCardDeck);
+
+        System.out.println(gameCardDeck.cardCount());
+
+    }
+
+
+
+}
+
