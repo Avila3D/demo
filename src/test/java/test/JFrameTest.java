48bca88aaddcb4997f74f4718e79d91e2c109607
Avila3D <tony@avila3d.com>
2019-03-23 00:07:15 -0600

    added title and not resizable code

--- src/test/java/test/JFrameTest.java
+++ src/test/java/test/JFrameTest.java
@@ -11,8 +11,10 @@ public class JFrameTest {
         FrameTest content = new FrameTest();
 
         //set properties
+        frame.setTitle("ASCII Viking Riding Dragon");
         frame.setVisible(true);
         frame.setSize(new Dimension(760,480));
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(content);
